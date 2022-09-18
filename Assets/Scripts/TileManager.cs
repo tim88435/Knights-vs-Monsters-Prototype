f13e7d80c54e856a380c86816f1ccde140b58bc5
tim88435 <timofey.kond@gmail.com>
2022-09-18 20:29:40 +1000

    Tile Manager done

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+    private static TileManager _singleton;
+    public static TileManager Singleton
     {
-        
+        get { return _singleton; }
+        set
+        {
+            if (_singleton == null)
+            {
+                _singleton = value;
+            }
+            else if (_singleton != value)
+            {
+                Debug.LogWarning($"{typeof(TileManager)} already exists in the current scene!\nRemoving Duplicate");
+                DestroyImmediate(value);
+            }
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    #endregion
+    private void OnValidate()
     {
-        
+        Singleton = this;//singleton stuff
     }
+    [SerializeField] public Tile[] firstTiles = new Tile[6];
 }
